7c46bea5a70fd9f1a5fe634331151832371c184a
vijaychopadexp <meetvjchopade@gmail.com>
2023-04-30 22:09:52 +0530

    war build optionn

---
+++ src/main/java/com/test/foodorder/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.test.foodorder;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(FoodOrderApplication.class);
+	}
+	
+	
+}
